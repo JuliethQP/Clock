85c22f34efee645e36335e7f1c0e1c925a6d6c56
Julieth <juliethquinchia@gmail.com>
2020-04-12 23:38:31 -0500

    Updates

---
+++ precache-manifest.f8cc05bd67d47383bf7b89ccbd2bb090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3719c696b38d37765b6db4f086172df3",
+    "url": "/Clock/index.html"
+  },
+  {
+    "revision": "476271cb99d79d7b1691",
+    "url": "/Clock/static/css/main.9fee0783.chunk.css"
+  },
+  {
+    "revision": "93dc9bf9c532edabdc2b",
+    "url": "/Clock/static/js/2.72a6824d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clock/static/js/2.72a6824d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476271cb99d79d7b1691",
+    "url": "/Clock/static/js/main.e33b5eb5.chunk.js"
+  },
+  {
+    "revision": "c53a2ad6f1f3b86a268c",
+    "url": "/Clock/static/js/runtime-main.f935e04d.js"
+  }
+]);
